279c38dcb52ef875128f9fadf28de8d028196076
Nick Alico <nro337@gmail.com>
2021-10-15 20:14:24 -0400

    Detail tag setup

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
 import { LearningCard } from './LearningCard.js';
+import { SciCard } from './SciCard.js';
 
 customElements.define(LearningCard.tag, LearningCard);
+customElements.define(SciCard.tag, SciCard);
