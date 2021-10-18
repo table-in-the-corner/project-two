680bc0447c2bbeadb592fc822e07c9ac3bbc6a81
njd5435 <89604655+njd5435@users.noreply.github.com>
2021-10-17 23:04:11 -0400

    icon initial

---
+++ src/sci-card-icon.js
@@ -0,0 +1,26 @@
+import { html, css } from 'lit';
+import { SimpleColors } from "@lrnwebcomponents/simple-colors/simple-colors.js";
+
+export class LearningIcon extends SimpleColors {
+    static get tag() {
+        return 'sci-card-icon';
+    }
+    constructor () {
+        super();
+    }
+    static get styles() {
+        return [...super.styles,
+            css`
+            :host {
+                display: block;
+                background-color: var(--simple-colors-default-theme-accent-1);
+                border: 2px solid orange;
+            }
+            `
+        ];
+    }
+    render() {
+        return html`<div>Icon Stuff<slot></slot><div>`;
+    }
+}
+window.customElements.define(LearningIcon.tag, LearningIcon);
